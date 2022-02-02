c75f9851a86c5a37970afa0c55d8e36ee4059132
Denis <varenikovda@gmail.com>
2022-02-02 11:35:08 +0300

    Update file ViewController and project

--- AuthorizationTool/ViewController.swift
+++ AuthorizationTool/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let userName = "Alexey"
+    let passwordUser = "Efimov"
 
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        passwordTF.text = ""
     }
 
-
+    @IBAction func showUserName() {
+        showAlert(title: "Reminder", massage: "Your name - \(userName)")
+    }
+    
+    @IBAction func showPassword() {
+        showAlert(title: "Reminder", massage: "Your password - \(passwordUser)")
+    }
+    
+    
 }
 
+// MARK: - Private Methods
+extension ViewController {
+    private func showAlert(title: String, massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTF.text = ""
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
